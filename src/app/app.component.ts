25ddec39c2b862a4daaff8e82fba035a37c3eade
Jared Lutz and Matt Nardoni <jared@>
2018-05-03 16:20:39 -0700

    trying to seperate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Keg } from './models/keg.model';
 export class AppComponent {
   title = 'Brewfest';
   header: string = "Brewery Bro's";
-  kegs: Keg[] = [
+  selectedKeg: Keg = this.childKegList[0];
+  masterKegList: Keg[] = [
     new Keg("Guinness", "Stout", "$5.00", "ABV: 3.8%", 124, 3 ),
     new Keg("Newcastle", "Ale", "$4.00", "ABV: 4.0%", 124, 2 ),
     new Keg("Yuengling", "Lager", "$2.50", "ABV: 2.5%", 124, 1 )
   ];
-  selectedKeg: Keg = this.kegs[0];
 
   sellKeg(clickedKeg) {
     this.selectedKeg = clickedKeg;
--- src/app/keg-list/keg-list.component.ts
+++ src/app/keg-list/keg-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Keg } from '../models/keg.model'
 
 @Component({
@@ -7,13 +7,10 @@ import { Keg } from '../models/keg.model'
   styleUrls: ['./keg-list.component.css']
 })
 export class KegListComponent {
-  kegs: Keg[] = [
-    new Keg("Guinness", "Stout", "$5.00", "ABV: 3.8%", 124, 3 ),
-    new Keg("Newcastle", "Ale", "$4.00", "ABV: 4.0%", 124, 2 ),
-    new Keg("Yuengling", "Lager", "$2.50", "ABV: 2.5%", 124, 1 )
-  ];
-  selectedKeg: Keg = this.kegs[0];
-  sellKeg(clickedKeg) {
+  @Input() childKegList: Keg[];
+  @Output() clickSender = new EventEmitter();
+  // selectedKeg: Keg = this.childKegList[0];
+  sellButtonClicked(clickedKeg) {
     this.selectedKeg = clickedKeg;
     clickedKeg.pints -= 1;
     if(clickedKeg.pints <= 10){
